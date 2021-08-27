d465b80b62610b514d4481714ecf47dec364e6c8
Eugene Bondarenco <evgenfsf@protonmail.com>
2021-08-27 10:30:21 -0400

    Trying FastAPI post implementation

--- api/waste.py
+++ api/waste.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from wasteassist.predict import build_model, predict
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+import os
 
 model = build_model()
 
@@ -18,6 +21,14 @@ app.add_middleware(
 @app.get("/")
 def index():
     return {"greeting": "Welcome to Waste Assist API."}
+
+@app.post("/files")
+async def receive_file(file: UploadFile = File(...)):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    filename = f'{dir_path}/uploads/{file.filename}'
+    # f = open(f'{filename}', 'wb')
+    content = await file.read()
+    # f.write(content)
 
 @app.get("/predict")
 def return_class(image):
