a4dbe9508273fa64dccc9f31aed9ef5518d9c2e3
Luis Paredes <paredeslm.ca@gmail.com>
2021-08-31 16:24:19 -0400

    Added config.toml

--- setup.sh
+++ setup.sh
@@ -10,4 +10,10 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+[theme]
+primaryColor = '#575fe8'/n
+backgroundColor = '#f5f7f3'\n
+secondaryBackgroundColor = '#c9eab8'\n
+textColor = '#262730'\n
+font = 'sans serif'\n
 " > ~/.streamlit/config.toml
